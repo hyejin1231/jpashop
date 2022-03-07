3f45a64be83044ef45cf6d772a1e104e26aeaaf8
hyejin1231 <phj981231@naver.com>
2022-03-07 21:04:55 +0900

    20220307 스프링 부트와 JPA 활용 1편 #4. 엔티티 클래스 개발(Order, OrderItem, Member, Address, Delivery, Item(Book, Movie, Album)

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 싱글 테이블 (한테이블에 다 떼려박는거)
+@DiscriminatorColumn(name = "dtype")
+@Getter
+@Setter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+
+}
