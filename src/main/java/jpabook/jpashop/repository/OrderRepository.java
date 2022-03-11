dd3ef342809b01625950f67cb73ca6e600efa1eb
hyejin1231 <phj981231@naver.com>
2022-03-11 22:41:15 +0900

    20220310 스프링 부트와 JPA 활용 1편 #17. 주문 검색 기능 ++

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,5 +20,15 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch)
+    public List<Order> findAll(OrderSearch orderSearch) {
+       return  em.createQuery("select o from Order o join o.member m" +
+                "where o.status = :status " +
+                "and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult() // 페이징
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
+
 }
